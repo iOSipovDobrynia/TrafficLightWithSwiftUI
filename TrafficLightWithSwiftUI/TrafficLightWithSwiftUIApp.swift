8829d429b53c7ab7859bb4623d58679197c8d689
iOSipovDobrynia <dobrynia.osipov@gmail.com>
2023-10-06 17:13:57 +0300

    Initial Commit

---
+++ TrafficLightWithSwiftUI/TrafficLightWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightWithSwiftUIApp.swift
+//  TrafficLightWithSwiftUI
+//
+//  Created by Goodwasp on 06.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
